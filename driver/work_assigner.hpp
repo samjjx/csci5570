d2dc9b9109a2cdfdb63e377e53c7fa80da3e7a12
liujianlin <liujianlin@imsightmed.com>
2018-11-12 14:53:47 +0800

    add missing file

---
+++ driver/work_assigner.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "base/magic.hpp"
+
+namespace csci5570 {
+  struct DataRange {
+    DataRange(uint32_t start_, uint32_t end_)
+      : start(start_), end(end_) {};
+    uint32_t start;
+    uint32_t end;
+  };
+
+  class WorkAssigner {
+  public:
+    WorkAssigner() = delete;
+    WorkAssigner(DataRange range) : range_(range) {
+      iter_num = 0;
+      cur_sample = 0;
+    };
+    uint32_t next_sample() {
+      if (cur_sample == range_.end) {
+        cur_sample = 0;
+        iter_num++;
+        return -1;
+      }
+      return cur_sample++;
+    }
+    DataRange get_data_range() {
+      return range_;
+    }
+  private:
+    DataRange range_;
+    uint32_t iter_num;
+    uint32_t cur_sample;
+  };
+}
